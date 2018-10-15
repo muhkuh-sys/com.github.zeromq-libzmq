019ab6a24921389da8aef9cfd9908f1b3af086a8
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2018-10-15 17:14:29 +0200

    Initial import.

---
+++ zeromq/installer/install_linux.lua
@@ -0,0 +1,11 @@
+local t = ...
+local strDistId, strDistVersion, strCpuArch = t:get_platform()
+local tResult
+
+if strDistId=='@JONCHKI_PLATFORM_DIST_ID@' and strDistVersion=='@JONCHKI_PLATFORM_DIST_VERSION@' and strCpuArch=='@JONCHKI_PLATFORM_CPU_ARCH@' then
+  t:install_dev('include',  '${install_dev_include}/')
+  t:install_dev('lib',      '${install_dev_lib}/')
+  tResult = true
+end
+
+return tResult
